7f8569e6251f998457d1fd67e5e570ad1d52e759
kxhitiz <nirvanagrg@gmail.com>
2014-11-08 12:07:14 +0545

    Every user has their own doc folder.

--- lib/document_processor.rb
+++ lib/document_processor.rb
@@ -10,7 +10,7 @@ class DocumentProcessor
   end
 
   def split_doc
-    Docsplit.extract_images(document, size: '1000x', format: [:png, :jpg], output: OUTPUT_PATH)
+    Docsplit.extract_images(document, size: '1000x', format: [:png, :jpg], output: "#{OUTPUT_PATH}/#{user.id}")
   end
 
   # returns either user object or just info hash
