46eda76e4c88b0708f5c78e3aafd48df229eb99b
kxhitiz <nirvanagrg@gmail.com>
2014-11-08 15:05:31 +0545

    Remove binding.pry.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-binding.pry
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
