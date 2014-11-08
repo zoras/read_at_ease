bb8252cb206706dc6f9fa4fac43fb0a3e58846ac
kxhitiz <nirvanagrg@gmail.com>
2014-11-08 11:28:58 +0545

    Library addition for processing document.

---
+++ lib/document_processor.rb
@@ -0,0 +1,18 @@
+class DocumentProcessor
+  attr_accessor :document, :user
+  
+  def initialize(document, user_hash)
+    @document = document 
+    @user = get_user(user_hash)
+  end
+
+  def split_doc
+    Docsplit.extract_images(document, :size => '1000x', :format => [:png, :jpg])
+  end
+
+  # returns either user object or just info hash
+  def get_user(user_hash)
+    user = User.find_or_initialize_by(email: user_hash['email'])
+    user if user.save
+  end
+end
