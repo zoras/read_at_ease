a7253b770137eb44efd5422d75be5b4d1e4d50f5
kxhitiz <nirvanagrg@gmail.com>
2014-11-08 12:03:56 +0545

    For now, force save user.

--- lib/document_processor.rb
+++ lib/document_processor.rb
@@ -1,18 +1,22 @@
 class DocumentProcessor
   attr_accessor :document, :user
 
+  OUTPUT_PATH = 'public/processed_documents'
+
+
   def initialize(document, user_hash)
     @document = document
     @user = get_user(user_hash)
   end
 
   def split_doc
-    Docsplit.extract_images(document, size: '1000x', format: [:png, :jpg], output: 'public/processed_documents')
+    Docsplit.extract_images(document, size: '1000x', format: [:png, :jpg], output: OUTPUT_PATH)
   end
 
   # returns either user object or just info hash
   def get_user(user_hash)
+    user_hash = user_hash.with_indifferent_access
     user = User.find_or_initialize_by(email: user_hash['email'])
-    user if user.save
+    user if user.save(validate: false)
   end
 end
