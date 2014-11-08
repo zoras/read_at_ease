6a6d234c2a5f0d7f72a63a080e3567243b0042d0
zoras <sawrows@gmail.com>
2014-11-08 13:02:46 +0545

    Document model added. User has_many documents

---
+++ app/models/document.rb
@@ -0,0 +1,12 @@
+class Document
+  include Mongoid::Document
+
+  ## Attributes
+  field :title,             type: String
+  field :location,          type: String, default: DocumentProcessor::OUTPUT_PATH
+  field :customize_time,    type: Time, default: nil
+  field :completed_reading, type: Boolean, default: false
+
+  ## Association
+  belongs_to :user
+end
